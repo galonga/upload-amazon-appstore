bf488625d84bebaf0df64c45091366334ad51dad
Timo Arndt <galonga@users.noreply.github.com>
2022-04-19 11:45:05 +0200

    Update README.md

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,127 @@
-import * as core from '@actions/core'
-import {wait} from './wait'
+const core = require('@actions/core')
+const fetch = require('node-fetch')
+const fs = require('fs')
 
-async function run(): Promise<void> {
+async function run() {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const clientId = core.getInput('clientId')
+    const clientSecret = core.getInput('clientSecret')
+    const appId = core.getInput('appId')
+    const apkFile = core.getInput('releaseFile')
+    const baseUrl = 'https://developer.amazon.com/api/appstore'
+    let editId, apkId, eTag
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    function handleErrors(response) {
+      if (!response.ok) throw Error(`[Error] ${response.statusText}`)
+      return response
+    }
 
-    core.setOutput('time', new Date().toTimeString())
+    core.info('Getting Authentication Token')
+
+    const authTokenResponse = await fetch('https://api.amazon.com/auth/o2/token', {
+      method: 'POST',
+      body: `grant_type=client_credentials&client_id=${clientId}&client_secret=${clientSecret}&scope=appstore::apps:readwrite`,
+      headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+    })
+      .then(handleErrors)
+      .then((response) => response.json())
+      .catch((error) => {
+        core.setFailed(error.message)
+      })
+
+    const authHeader = `Bearer ${authTokenResponse.access_token}`
+
+    core.info('Checking if an open Edit exists')
+
+    const getActiveEditResponse = await fetch(`${baseUrl}/v1/applications/${appId}/edits`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: authHeader,
+      },
+    })
+      .then(handleErrors)
+      .then((response) => response.json())
+      .catch((error) => {
+        core.setFailed(error.message)
+      })
+
+    if (JSON.stringify(getActiveEditResponse) === '{}') {
+      core.info('No active edit found, creating new one')
+
+      const createEditResponse = await fetch(`${baseUrl}/v1/applications/${appId}/edits`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: authHeader,
+        },
+      })
+        .then(handleErrors)
+        .then((response) => response.json())
+        .catch((error) => {
+          core.setFailed(error.message)
+        })
+
+      core.info('Creating new Edit')
+      editId = createEditResponse.id
+    } else {
+      core.info('Open edit found')
+      editId = getActiveEditResponse.id
+    }
+
+    await fetch(`${baseUrl}/v1/applications/${appId}/edits/${editId}/apks`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: authHeader,
+      },
+    })
+      .then(handleErrors)
+      .then((response) => response.json())
+      .then((data) => {
+        for (const apk of data) {
+          core.info(`Found apk with version code ${apk.versionCode} - id ${apk.id} - ${apk.name}`)
+          apkId = apk.id
+        }
+      })
+      .catch((error) => {
+        core.setFailed(error.message)
+      })
+
+    await fetch(`${baseUrl}/v1/applications/${appId}/edits/${editId}/apks/${apkId}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: authHeader,
+      },
+    })
+      .then(handleErrors)
+      .then((response) => {
+        eTag = response.headers.get('etag')
+      })
+      .catch((error) => {
+        core.setFailed(error.message)
+      })
+
+    await fetch(`${baseUrl}/v1/applications/${appId}/edits/${editId}/apks/${apkId}/replace`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/vnd.android.package-archive',
+        Authorization: authHeader,
+        'If-Match': eTag,
+      },
+      body: fs.createReadStream(apkFile),
+    })
+      .then(handleErrors)
+      .then((response) => {
+        core.info('Successfully uploaded apk')
+      })
+      .catch((error) => {
+        core.setFailed(`Failed to upload apk due to ${error.message}`)
+      })
   } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
+    core.setFailed(`[Error] There was an error with the action: ${error}`)
   }
-}
+};
 
-run()
+run();
